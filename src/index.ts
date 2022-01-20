27d4a4eb8c1901bc03065b3a5bf401712532ccd1
Timur Badretdinov <destinerx@gmail.com>
2022-01-20 11:18:21 +0300

    Export types

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,20 @@
 import Editor from './components/Editor.vue';
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
+import type { File } from './components/FileSelector.vue';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
 import Sandbox from './components/Sandbox.vue';
+import type {
+	SandpackTheme,
+	SandpackPredefinedTheme,
+	SandpackSyntaxStyle,
+} from './themes';
 
 export { Editor, Sandbox };
+export type {
+	File,
+	SandpackTheme,
+	SandpackPredefinedTheme,
+	SandpackSyntaxStyle,
+};
